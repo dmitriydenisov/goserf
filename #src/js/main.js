176781e6e19a51a4b41c8d6e1ae440541e42beb2
Dmitrii Denisov <dmitriyd2009@yandex.ru>
2021-01-22 00:15:37 +0400

    верстка секций-секция SLEEP-программирование калькулятора

--- #src/js/main.js
+++ #src/js/main.js
@@ -34,7 +34,41 @@ $(function (){
         fade:true,
         prevArrow: '<img class="slider-arrow slider-arrow__left" src="img/icons/arrow-left.svg"></img>',
         nextArrow: '<img class="slider-arrow slider-arrow__right" src="img/icons/arrow-right.svg"></img>',
-    })
+    });
+
+
+    $('<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="img/icons/plus.svg"></img></div><div class="quantity-button quantity-down"><img src="img/icons/minus.svg"></img></div></div>').insertAfter('.quantity input');
+    $('.quantity').each(function() {
+      var spinner = $(this),
+        input = spinner.find('input[type="number"]'),
+        btnUp = spinner.find('.quantity-up'),
+        btnDown = spinner.find('.quantity-down'),
+        min = input.attr('min'),
+        max = input.attr('max');
+
+      btnUp.click(function() {
+        var oldValue = parseFloat(input.val());
+        if (oldValue >= max) {
+          var newVal = oldValue;
+        } else {
+          var newVal = oldValue + 1;
+        }
+        spinner.find("input").val(newVal);
+        spinner.find("input").trigger("change");
+      });
+
+      btnDown.click(function() {
+        var oldValue = parseFloat(input.val());
+        if (oldValue <= min) {
+          var newVal = oldValue;
+        } else {
+          var newVal = oldValue - 1;
+        }
+        spinner.find("input").val(newVal);
+        spinner.find("input").trigger("change");
+      });
+
+    });
 });
 
 
