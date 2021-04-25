f4d1a8f0fcbc1542dbe924c0ff8debc20c49a7ba
Dmitrii Denisov <dmitriyd2009@yandex.ru>
2021-04-25 22:33:55 +0400

    оптимизация JS

--- #src/js/initlibs.js
+++ #src/js/initlibs.js
@@ -83,7 +83,7 @@ $(function (){
     $('<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="img/icons/plus.svg"></img></div><div class="quantity-button quantity-down"><img src="img/icons/minus.svg"></img></div></div>').insertAfter('.quantity input');
     $('.quantity').each(function() {
       var spinner = $(this),
-        input = spinner.find('input[type="number"]'),
+        input = spinner.find('input[type="number"]'), 
         btnUp = spinner.find('.quantity-up'),
         btnDown = spinner.find('.quantity-down'),
         min = input.attr('min'),
