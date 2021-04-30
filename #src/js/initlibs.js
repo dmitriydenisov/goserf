0014c59f6b74b83d102922ed5aa500a79512b6bb
Dmitrii Denisov <dmitriyd2009@yandex.ru>
2021-05-01 01:21:43 +0400

    изменение скрипта

--- #src/js/initlibs.js
+++ #src/js/initlibs.js
@@ -80,47 +80,7 @@ $(function (){
     });
 
 
-    $('.quantity').each(function() {
-      var spinner = $(this),
-        input = spinner.find('input[type="number"]'),
-        btnUp = spinner.find('.quantity-up'),
-        btnDown = spinner.find('.quantity-down'),
-        min = input.attr('min'),
-        max = input.attr('max');
-
-      btnUp.click(function() {
-        var oldValue = parseFloat(input.val());
-        if (oldValue >= max) {
-          var newVal = oldValue;
-        } else {
-          var newVal = oldValue + 1;
-        }
-        spinner.find("input").val(newVal);
-        spinner.find("input").trigger("change");
-      });
-
-      btnDown.click(function() {
-        var oldValue = parseFloat(input.val());
-        if (oldValue <= min) {
-          var newVal = oldValue;
-        } else {
-          var newVal = oldValue - 1;
-        }
-        spinner.find("input").val(newVal);
-        spinner.find("input").trigger("change");
-      });
-
-    });
-
-
-
-let summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val() - 1) * $('.summ').data('guests');
-$('.summ').html('$' + summ);
-
-$('.quantity-button').on('click', function () {
-  let summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val() - 1) * $('.summ').data('guests');
-  $('.summ').html('$' + summ);
-})
+    
 
 
 new WOW().init();
